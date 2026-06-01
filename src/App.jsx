45d344c4b33eb2e840f5801f969395878a0fad69
Stephen Muiga <stevekiragu254@gmail.com>
2026-06-01 23:40:08 +0300

    cart actions

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,22 @@ import Checkout from "./pages/Checkout";
 import Auth from "./pages/Auth";
 import Navbar from "./components/Navbar";
 import AuthProvider from "./context/AuthContext";
+import ProductDetail from "./pages/ProductDetail";
+import CartProvider from "./context/CartContext";
 function App() {
   return (
     <AuthProvider>
-      <div className="app">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className="app">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/products/:id" element={<ProductDetail />} />
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
   );
 }
